6f66299a4e73e809fed865d2c6db335ab0e79d29
jafetpintle <jafetpintle@gmail.com>
2026-04-20 22:31:08 -0600

    generating accounting processor

---
+++ accounting-processor/accounting-processor/src/main/java/japi/accounting_processor/AccountingProcessorApplication.java
@@ -0,0 +1,13 @@
+package japi.accounting_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountingProcessorApplication.class, args);
+	}
+
+}
---
+++ accounting-processor/accounting-processor/src/test/java/japi/accounting_processor/AccountingProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package japi.accounting_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
