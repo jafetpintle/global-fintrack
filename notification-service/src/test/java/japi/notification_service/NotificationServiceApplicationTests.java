db80a108695e6e30b1dfc2808278fd6b2c07937d
jafetpintle <jafetpintle@gmail.com>
2026-04-26 22:33:46 -0600

    notification service base implementation

---
+++ notification-service/src/test/java/japi/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package japi.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
