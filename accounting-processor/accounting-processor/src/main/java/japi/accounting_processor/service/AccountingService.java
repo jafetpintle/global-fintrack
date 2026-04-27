752ec26fc64c17bb5edb88dd9068190e65fce3ab
jafetpintle <jafetpintle@gmail.com>
2026-04-26 23:55:47 -0600

    user not found for transaction proccesing

--- accounting-processor/accounting-processor/src/main/java/japi/accounting_processor/service/AccountingService.java
+++ accounting-processor/accounting-processor/src/main/java/japi/accounting_processor/service/AccountingService.java
@@ -7,7 +7,6 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import japi.accounting_processor.dto.TransactionEvent;
-import japi.accounting_processor.entity.Account;
 import japi.accounting_processor.repository.AccountRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,26 +22,31 @@ public class AccountingService {
     @Bean
     public Consumer<TransactionEvent> processTransaction() {
         return event -> {
-            log.info("Processing in db to user: {}", event.userId());
-            // Postgres persitence
-            Account account = repository.findById(event.userId())
-                .orElse(new Account(event.userId(), java.math.BigDecimal.ZERO, 0L));
+            repository.findById(event.userId()).ifPresentOrElse(
+                account -> {
+                    log.info("Processing in db to user: {}", event.userId());
+                    // Postgres persitence
 
-            if ("DEPOSIT".equals(event.type())) {
-                account.setBalance(account.getBalance().add(event.amount()));
-            } else {
-                account.setBalance(account.getBalance().subtract(event.amount()));
-            }
+                    if ("DEPOSIT".equals(event.type())) {
+                        account.setBalance(account.getBalance().add(event.amount()));
+                    } else {
+                        account.setBalance(account.getBalance().subtract(event.amount()));
+                    }
 
-            repository.save(account);
-            log.info("New balance to {}: {}", event.userId(), account.getBalance());
+                    repository.save(account);
+                    log.info("New balance to {}: {}", event.userId(), account.getBalance());
 
-            //Redis syncronising
-            //Saving balance with 10 minutes expiration to dont fill RAM
-            String cacheKey = "balance:" + event.userId();
-            redisTemplate.opsForValue().set(cacheKey, account.getBalance().toString(), java.time.Duration.ofMinutes(10));
+                    //Redis syncronising
+                    //Saving balance with 10 minutes expiration to dont fill RAM
+                    String cacheKey = "balance:" + event.userId();
+                    redisTemplate.opsForValue().set(cacheKey, account.getBalance().toString(), java.time.Duration.ofMinutes(10));
 
-            log.info("Redis cache updated! New balance in RAM: {}", account.getBalance());
+                    log.info("Redis cache updated! New balance in RAM: {}", account.getBalance());
+                },
+                () -> {
+                    log.error("ERROR: User {} not found. Aborted transaction", event.userId());
+                }
+            );
         };
     }
 }
